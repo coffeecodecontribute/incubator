54a04dd3597ac90b9f59d407e6321dd5808be055
Soeren Winterhalder <wiswirlp@gmail.com>
2018-07-24 12:23:12 +0200

    feat: added article

--- packages/pinder-frontend/src/index.js
+++ packages/pinder-frontend/src/index.js
@@ -4,12 +4,14 @@ import { Navigation } from "./components/Navigation";
 import { Image } from "./components/Image";
 import { Footer } from "./components/Footer";
 import { Content } from "./components/Content";
+import { Article } from "./components/Article";
 
 ReactDOM.render(
   <Fragment>
     <Navigation />
     <Content />
     <Image />
+    <Article/>
     <Footer />
   </Fragment>,
   document.getElementById("root")
