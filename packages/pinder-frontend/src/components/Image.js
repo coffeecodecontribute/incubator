938280766e8ccf5acf2e7f80a7988ac3b46b927c
Soeren Winterhalder <wiswirlp@gmail.com>
2018-07-24 12:33:40 +0200

    feat: nice button

--- packages/pinder-frontend/src/components/Image.js
+++ packages/pinder-frontend/src/components/Image.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Image = () => (
   <img
-    alt="Panda"
+    alt="This is a Panda"
     src="https://www.seokratie.de/wp-content/uploads/2014/05/panda-800x593.jpg"
   />
 );
