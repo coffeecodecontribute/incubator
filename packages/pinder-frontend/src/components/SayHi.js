0a9963f021432264954e9030022dbc464ed0ee4f
Soeren Winterhalder <wiswirlp@gmail.com>
2018-07-24 12:37:22 +0200

    feat: nice button

--- packages/pinder-frontend/src/components/SayHi.js
+++ packages/pinder-frontend/src/components/SayHi.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+function sayHello() {
+    alert("Hello, welcome to Pinder");
+    return;
+}
+
 export const SayHi = () => (
-    <input type="submit" value="Say Hello" onclick="alert('Hello)"/>
+    <input type="submit" value="Say Hello" onclick="alert('Hello')"/>
 )
