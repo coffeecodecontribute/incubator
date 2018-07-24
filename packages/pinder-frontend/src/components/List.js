769063da83d4e0334699bc5f3d84f2a23e30653c
Paul Tebbe <paulcannot@gmail.com>
2018-07-24 12:47:41 +0200

    feat: add List component

---
+++ packages/pinder-frontend/src/components/List.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const List = () => (
+  <ul>
+    <li>Design your own Panda-Profile</li>
+    <li>Meet nice and sweet Pandas</li>
+    <li>Comment and like your friends</li>
+  </ul>
+);
