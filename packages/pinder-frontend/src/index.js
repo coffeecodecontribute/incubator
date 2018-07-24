f2691bbb311e82273f5179d30fb2778854c64ba1
Soeren Winterhalder <wiswirlp@gmail.com>
2018-07-24 12:18:06 +0200

    feat: Image(s) of Panda(s).

--- packages/pinder-frontend/src/index.js
+++ packages/pinder-frontend/src/index.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Navigation } from "./components/Navigation";
+import { Image } from "./components/Image";
 import { Footer } from "./components/Footer";
 import { Content } from "./components/Content";
 
@@ -8,6 +9,7 @@ ReactDOM.render(
   <Fragment>
     <Navigation />
     <Content />
+    <Image />
     <Footer />
   </Fragment>,
   document.getElementById("root")
