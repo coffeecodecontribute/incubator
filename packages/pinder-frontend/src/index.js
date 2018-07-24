a7dc11b1477bc70880b30ada297d43c65e4cc3ac
Pascal Sysala <pascal.sysala@gmail.com>
2018-07-24 12:17:50 +0200

    feat:  added Article component

--- packages/pinder-frontend/src/index.js
+++ packages/pinder-frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom";
 import { Navigation } from "./components/Navigation";
 import { Footer } from "./components/Footer";
 import { Content } from "./components/Content";
+import { Article } from "./components/Article";
 
 ReactDOM.render(
   <Fragment>
     <Navigation />
     <Content />
+    <Article/>
     <Footer />
   </Fragment>,
   document.getElementById("root")
